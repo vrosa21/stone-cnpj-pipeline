0f03af915bbb63a0f050da112e416457a0dae368
Vinicius Rosa Ribeiro <vinicius.rosaribeiro@gmail.com>
2025-09-29 02:34:00 -0300

    atualizacao_stone_final

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# Inicia o cron daemon em primeiro plano para manter o container vivo
-cron -f
+# Inicia o cron daemon em primeiro plano para manter o container vivo e executar tarefas agendadas
+cron -f 
